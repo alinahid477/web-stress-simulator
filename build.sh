544863fb386ad3e0507402ab54c29fcca4a14a8a
Ali Nahid <alinahid@gmail.com>
2021-11-17 09:36:33 +1100

    webstress my customisation + k8s deployment

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ BUILD_VERSION=1.0.0
 
 echo "Building web-stress-simulator $BUILD_VERSION..."
 
-docker build -t flaviostutz/web-stress-simulator:$BUILD_VERSION .
+docker build -t alinahid477/web-stress-simulator:$BUILD_VERSION .
 
 echo "Finished building web-stress-simulator."
